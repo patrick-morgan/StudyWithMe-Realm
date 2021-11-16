4c8c16b79adb617f174c0ed61eae81a2f1e33a5e
pmorg <pmorg@umich.edu>
2021-11-16 17:29:15 -0500

    backend support for adding new friends

---
+++ functions/addFriend.js
@@ -0,0 +1,32 @@
+exports = function(userIdToUpdate, friendId, newRelationshipType){
+  /*
+    Accessing application's values:
+    var x = context.values.get("value_name");
+
+    Accessing a mongodb service:
+    var collection = context.services.get("mongodb-atlas").db("dbname").collection("coll_name");
+    collection.findOne({ owner_id: context.user.id }).then((doc) => {
+      // do something with doc
+    });
+
+    To call other named functions:
+    var result = context.functions.execute("function_name", arg1, arg2);
+
+    Try running in the console below.
+  */
+
+  const db = context.services.get("mongodb-atlas").db("dev");
+  const userCollection = db.collection("User");
+  
+  // friendId is actually the current user calling this function
+  // userIdToUpdate is the other user they interacted with
+  // we need to update the relationship w/ friendId == friendId for userIdToUpdate on the backend
+  // since this user doesn't have permission to make this change from the client
+  return userCollection.updateOne({ "_id": userIdToUpdate }, {$addToSet : {friends: {id: BSON.ObjectId().toString(), friendId: friendId, relationshipType: newRelationshipType}}})
+  .then(result => {
+    console.log(`Added new user relationship for ${userIdToUpdate}, updated friendId ${friendId} to ${newRelationshipType}`);
+  }, error => {
+    console.log(`Failed to add new relationship for user ${userIdToUpdate}, tried to add friendId ${friendId} to ${newRelationshipType}`);
+    console.log(error);
+  });
+};
